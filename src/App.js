9a426dafe9b64616d93a9f65a20bc4b65cf44146
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-04 18:59:41 -0300

    Render in React Based on a Global State Object

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React Todo App with Redux - Legacy</p>
-      </header>
-      <div className="Todo-App">
-        <form>
-          <input type="text" />
-        </form>
-        <div className="Todo-List">
-          <ul>
-            <li>
-              <input type="checkbox" /> Create Static UI
-            </li>
-            <li>
-              <input type="checkbox" /> Create Initial State
-            </li>
-            <li>
-              <input type="checkbox" /> Use State to Render UI
-            </li>
-          </ul>
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>React Todo App with Redux - Legacy</p>
+        </header>
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.props.todos.map((todo) => (
+                <li key={todo.id}>
+                  <input
+                    type="checkbox"
+                    defaultChecked={todo.isComplete}
+                  /> {todo.name}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const state = {
+  todos: [
+    { id: 1, name: "Render static UI", isComplete: true },
+    { id: 2, name: "Create initial state", isComplete: true },
+    { id: 3, name: "Render based on state", isComplete: false },
+  ]
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App todos={state.todos} />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
