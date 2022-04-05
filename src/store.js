2bfaa7bbb9e68e45f0ce4125a7b105dfdc819d2e
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-05 11:24:15 -0300

    Configure and Use Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevtools } from 'redux-devtools-extension'
 // thunk es un middleware
 import thunk from 'redux-thunk'
 import reducer from './reducers/todo'
@@ -7,5 +8,7 @@ import reducer from './reducers/todo'
 // con thunk puedo empezar a dispatch asyncronous actions
 export default createStore(
   reducer,
+  // imposible hacerlo funcionar con composeWithDevtools(applyMiddleware(thunk)). Asi si funciona: 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 );
