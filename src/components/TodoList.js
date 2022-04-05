59f51b5a8f1a9e320d26dbcfeb4cb86076c0efbd
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-05 13:42:46 -0300

    Deleting Existing Records on The Server with Redux Thunk and Fetch

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchTodos, toggleTodo } from "../reducers/todo";
+import { fetchTodos, toggleTodo, deleteTodo } from "../reducers/todo";
 
-const TodoItem = ({id, name, isComplete, toggleTodo}) => (
+const TodoItem = ({ id, name, isComplete, toggleTodo, deleteTodo }) => (
   <li>
+    <span className="delete-item">
+      <button onClick={() => deleteTodo(id)}>X</button>
+    </span>
     <input
       type="checkbox"
       checked={isComplete}
       onChange={() => toggleTodo(id)}
-    /> {name}
+    />{" "}
+    {name}
   </li>
-)
+);
 
 // vamos a cambiar nuestro TodoList component a una class component para poder usar el hook componentDidMount()
 // con fetchTodos que traigo del reducer
 class TodoList extends Component {
   componentDidMount() {
-    this.props.fetchTodos()
+    this.props.fetchTodos();
   }
 
   render() {
@@ -28,7 +32,9 @@ class TodoList extends Component {
             <TodoItem
               key={todo.id}
               toggleTodo={this.props.toggleTodo}
-              {...todo} />
+              deleteTodo={this.props.deleteTodo}
+              {...todo}
+            />
           ))}
         </ul>
       </div>
@@ -38,7 +44,8 @@ class TodoList extends Component {
 
 // hago el connect sin definir primer un "mapStateToProps" --> lo defino directamente como 1er argumento de connect
 // aqui no necesito action functions, por eso no pongo nada como 2do argumento
-export default connect(
-  (state) => ({ todos: state.todo.todos }),
-  {fetchTodos, toggleTodo}
-)(TodoList)
+export default connect((state) => ({ todos: state.todo.todos }), {
+  fetchTodos,
+  toggleTodo,
+  deleteTodo,
+})(TodoList);
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,5 +1,5 @@
 // llamadas a la API
-import { getTodos, createTodo, updateTodo } from "../lib/todoServices";
+import { getTodos, createTodo, updateTodo, destroyTodo } from "../lib/todoServices";
 import { showMessage } from "./messages";
 
 const initState = {
@@ -19,6 +19,7 @@ export const TODO_ADD = 'TODO_ADD'
 export const TODOS_LOAD = 'TODOS_LOAD'
 const CURRENT_UPDATE = 'CURRENT_UPDATE'
 export const TODO_REPLACE = 'TODO_REPLACE'
+export const TODO_REMOVE = 'TODO_REMOVE'
 
 
 // action creator functions --> retorno un objeto
@@ -26,31 +27,43 @@ export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val })
 export const loadTodos = (todos) => ({ type: TODOS_LOAD, payload: todos });
 export const addTodo = (todo) => ({ type: TODO_ADD, payload: todo });
 export const replaceTodo = (todo) => ({ type: TODO_REPLACE, payload: todo });
+export const removeTodo = (id) => ({ type: TODO_REMOVE, payload: id });
 
-// vamos a crear un nuevo action creator para trabajar con la API 
+// vamos a crear un nuevos ACTION CREATORS que primero trabajan con la API y luego modifica el state
 // con thunk voy a retornar una function. Luego lo aplico a un action creator 'loadTodos'
 // quien llama a fetchTodos es TodoList en su hook componentDidMount()
 export const fetchTodos = () => {
   return (dispatch) => {
     dispatch(showMessage('Loading Todos...'))
+    // primero llama a la API luego modifica el state
     getTodos().then(todos => dispatch(loadTodos(todos)))
   }
 }
 export const saveTodo = (name) => {
   return (dispatch) => {
-    dispatch(showMessage('Saving Todo'))
-    createTodo(name).then(res => dispatch(addTodo(res)));
+    dispatch(showMessage("Saving Todo"));
+    // primero llama a la API luego modifica el state
+    createTodo(name).then((res) => dispatch(addTodo(res)));
   };
 };
 export const toggleTodo = (id) => {
   return (dispatch, getState) => {
     dispatch(showMessage("Saving Todo update"));
-    const {todos} = getState().todo
-    const todo = todos.find(todo => todo.id === id);
-    const toggled = {...todo, isComplete: !todo.isComplete}
+    const { todos } = getState().todo;
+    const todo = todos.find((todo) => todo.id === id);
+    const toggled = { ...todo, isComplete: !todo.isComplete };
+    // primero llama a la API luego modifica el state
     updateTodo(toggled).then((res) => dispatch(replaceTodo(res)));
   };
 };
+export const deleteTodo = (id) => {
+  return (dispatch) => {
+    dispatch(showMessage("Deleting Todo"))
+    destroyTodo(id).then(() => dispatch(removeTodo(id)))
+  }
+}
+
+
 
 
 // export reducer
@@ -66,6 +79,13 @@ export default (state = initState, action) => {
       return {
         ...state,
         todos: state.todos.map(todo => todo.id === action.payload.id ? action.payload : todo)
+      };
+    case TODO_REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) =>
+          todo.id !== action.payload
+        ),
       };
     default:
       return state;
